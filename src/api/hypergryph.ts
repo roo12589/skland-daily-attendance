8c54a4a229af5b6f99d992415805f8f0e689eeb2
roo <roo12589@163.com>
2024-07-31 16:20:54 +0800

    Update hypergryph.ts

--- src/api/hypergryph.ts
+++ src/api/hypergryph.ts
@@ -7,18 +7,23 @@ import { command_header } from '../utils'
  * @param token 鹰角网络通行证账号的登录凭证
  */
 export async function auth(token: string) {
-  const response = await fetch(SKLAND_AUTH_URL, {
-    method: 'POST',
-    headers: command_header,
-    body: JSON.stringify({
-      appCode: '4ca99fa6b56cc2ba',
-      token,
-      type: 0,
-    }),
-  })
-  const data = await response.json() as AuthResponse
-  if (data.status !== 0 || !data.data)
-    throw new Error(`登录获取 cred 错误:${data.msg}`)
+    const response = await fetch(SKLAND_AUTH_URL, {
+        method: 'POST',
+        headers: command_header,
+        body: JSON.stringify({
+            appCode: '4ca99fa6b56cc2ba',
+            token,
+            type: 0,
+        }),
+    })
+    let data
+    try {
+        data = (await response.json()) as AuthResponse
+    } catch (e) {
+        console.log('fail!!!!', e, response)
+    }
+    if (data.status !== 0 || !data.data)
+        throw new Error(`登录获取 cred 错误:${data.msg}`)
 
-  return data.data
+    return data.data
 }
